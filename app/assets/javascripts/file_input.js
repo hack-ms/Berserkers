002e27ede232aefcf658c710579c38f0b37da48e
Vinicius da Silva <vssilva233@gmail.com>
2019-08-03 15:04:59 -0400

    style new complaint

---
+++ app/assets/javascripts/file_input.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+
+  $('#button').click(function(){
+    $("input[type='file']").trigger('click');
+  });
+
+  $("input[type='file']").change(function(){
+    $('#val').text(this.value.replace(/C:\\fakepath\\/i, ''));
+  });   
+});
