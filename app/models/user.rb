8cb3f4881ae0b837ece461544ae8f9e9073807ef
Vinicius da Silva <vssilva233@gmail.com>
2019-08-03 13:25:28 -0400

    add route to show user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   has_many :complaints, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
