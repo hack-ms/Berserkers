4e3d3ff7a58858520269fd93f96d3eb044146024
Vinicius da Silva <vssilva233@gmail.com>
2019-08-03 19:43:17 -0400

    add Licence.txt, update values score and quantity

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,12 +1,15 @@
 class ExchangesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_promo_code, only: [:create]
+  before_action :verify_points, only: [:create]
 
   def create 
     exchange = current_user.exchanges.create(promo_code: @promo_code)   
     
     if exchange.persisted?
       @promo_code.decrement_quantity!
+      current_user.update(score: current_user.score - @promo_code.required_points)
+
       flash[:notice] = "Sucesso! Verifique no seu email o código promocional"
     else
       flash[:error] = "Ocorreu um erro ao resgatar o código"
@@ -19,5 +22,12 @@ class ExchangesController < ApplicationController
     def set_promo_code
       @promo_code = PromoCode.find_by(id: params[:id])
       return redirect_to user_path(current_user) unless @promo_code.present?
+    end
+
+    def verify_points
+      if @promo_code.required_points > current_user.score
+        flash[:error] = "Você não tem pontos suficientes para este cupom :("
+        return redirect_to user_path(current_user) 
+      end
     end
 end
