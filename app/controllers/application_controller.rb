3d4930d5934fa20393dee98925ecefa63ece674f
Henrique Januário <riquejanuario@jera.com.br>
2019-08-22 12:20:23 -0400

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def matches
-    render json: HTTParty.get("https://globoesporte.globo.com/placar-ge/#{params[:date]}/jogos.ghtml").parsed_response
+    render json: HTTParty.get("https://globoesporte.globo.com/placar-ge/#{params[:date]}/jogos.ghtml")
   end
 
 end
